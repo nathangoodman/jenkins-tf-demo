fccc62be573ac1f9195d5a879e937328fe9ca15f
Nathan Goodman <nathangoodman@N8s-MacBook-Pro.local>
2019-03-17 11:11:42 -0400

    Added tags and versioning

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 resource "aws_s3_bucket" "this" {
   bucket = "${var.bucketname}"
   acl    = "private"
-
+  
+  versioning {
+    enabled = true
+  }
+  
+  tags = {
+    Name        = "N8 Demo Bucket"
+    Environment = "Demo"
+  }
 }
